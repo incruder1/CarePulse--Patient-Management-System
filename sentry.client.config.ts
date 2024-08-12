432c9f9fb21cc103ef1522073638994527f65736
Dev Johri <56020041+incruder1@users.noreply.github.com>
2024-08-12 13:47:20 +0530

    disable ts timebeing

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,14 +5,13 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3d627de24f5d06a1fc39000a06ca9a94@o4506813739368448.ingest.us.sentry.io/4507458386526208",
+  dsn: "https://00603076b2c0d5b88e95a7d3062a3020@o4507763276120064.ingest.de.sentry.io/4507763283918928",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
   replaysOnErrorSampleRate: 1.0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
--- types/index.d.ts
+++ types/index.d.ts
@@ -55,3 +55,4 @@ declare type UpdateAppointmentParams = {
   appointment: Appointment;
   type: string;
 };
+
